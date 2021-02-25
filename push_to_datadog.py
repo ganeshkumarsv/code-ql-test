06209012396c4c5d429ee0969a42f7486271bd3d
ganeshkumarsv <53483484+ganeshkumarsv@users.noreply.github.com>
2021-02-25 08:17:16 -0500

    Update push_to_datadog.py

--- push_to_datadog.py
+++ push_to_datadog.py
@@ -7,6 +7,7 @@ import requests
 url = 'https://http-intake.logs.datadoghq.com/v1/input'
 with open('/home/runner/work/code-ql-test/results/go-builtin.sarif') as f:
     json_obj = json.load(f)
-x = requests.post(url, data = json_obj, headers={'DD-API-KEY':os.environ["DD_API_KEY"], 'Content-Type':'application/json'})
+message = '{"message": ' + json.dumps(json_obj) + ', "ddtags":"from:codeql_results"}' 
+x = requests.post(url, data=message, headers={'DD-API-KEY':os.environ["DD_API_KEY"], 'Content-Type':'application/json'})
 
 print(x.text)
